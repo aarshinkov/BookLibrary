6e9eb3065e38c59ff561af27e52c8ecfbb23c44e
Atanas <a.arshinkov97@gmail.com>
2018-11-28 00:21:24 +0200

    Added entities

---
+++ BookLibrary/BookLibrary.Data/Context/BookCatalogDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+using BookLibrary.Data.Entity;
+
+namespace BookLibrary.Data.Context
+{
+    public class BookCatalogDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
---
+++ BookLibrary/BookLibrary.Data/Entity/Writer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookLibrary.Data.Entity
+{
+    public class Writer
+    {
+        [Key]
+        public int WriterId { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 1)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(200, MinimumLength = 1)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+
+    }
+}
